7b3d2e378bb3b6d4f599155b23e39c8788344893
miltan17 <miltan17.ruet@gmail.com>
2025-07-17 16:48:57 +0600

    Footer Added

---
+++ src/footer.jsx
@@ -0,0 +1,6 @@
+<div>
+    <h2>Title</h2>
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, beatae iste, obcaecati explicabo, consequatur voluptatem nulla cupiditate iure repellat pariatur doloremque dicta quaerat illum nemo! Soluta iste temporibus labore minima.
+    </p>
+</div>
